7e890057dd83db151db3f0da90b6a8d51366c292
Dinesh Uthayakumar <udinesh5@gmail.com>
2024-09-20 20:22:32 +0530

    Replacing Newtonsoft.JSON deserializer with .NET system library.

--- EventGridCloudEventTriggerFromAzureBlobStorage.cs
+++ EventGridCloudEventTriggerFromAzureBlobStorage.cs
@@ -1,49 +1,54 @@
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Azure.Messaging;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Azure.Messaging;
-using System;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace DU.BlobCreated
 {
     public class StorageDiagnostics
     {
-        public string BatchId { get; set; }
+        public string batchId { get; set; }
     }
 
     public class AzureBlobCloudEventData
     {
-        public string Api { get; set; }
-        public string ClientRequestId { get; set; }
-        public string RequestId { get; set; }
-        public string ETag { get; set; }
-        public string ContentType { get; set; }
-        public long ContentLength { get; set; }
-        public string BlobType { get; set; }
-        public string AccessTier { get; set; }
-        public string Url { get; set; }
-        public string Sequencer { get; set; }
-        public StorageDiagnostics StorageDiagnostics { get; set; }       
+        public string api { get; set; }
+        public string clientRequestId { get; set; }
+        public string requestId { get; set; }
+        public string eTag { get; set; }
+        public string contentType { get; set; }
+        public long contentLength { get; set; }
+        public string blobType { get; set; }
+        public string accessTier { get; set; }
+        public string url { get; set; }
+        public string sequencer { get; set; }
+        public StorageDiagnostics storageDiagnostics { get; set; }       
     }
 
     public static class EventGridCloudEventTriggerFromAzureBlobStorage
     {
         [FunctionName("EventGridCloudEventTriggerFromAzureBlobStorage")]
         public static async Task Run([EventGridTrigger]CloudEvent cloudEvent, ILogger log)
-        //public static void Run([EventGridTrigger]CloudEvent cloudEvent, ILogger log)
         {
-            var azureBlobCloudEventDataObject = JsonConvert.DeserializeObject<AzureBlobCloudEventData>(cloudEvent.Data.ToString());
-            log.LogInformation("URL received {url}", azureBlobCloudEventDataObject.Url);
+            var azureBlobCloudEventData = System.Text.Json.JsonSerializer.Deserialize<AzureBlobCloudEventData>(cloudEvent.Data.ToString());
+            log.LogInformation("URL deserialized {url}", azureBlobCloudEventData.url);
 
-            var url = "logic-app-https-listener-url";
-            var payload = "{\"url\": \"" + azureBlobCloudEventDataObject.Url + "\"}";
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.Add("url", azureBlobCloudEventData.url);
+            var content = new StringContent(jsonObject.ToJsonString(), Encoding.UTF8, "application/json");
+
+            var logicAppHttpRequestListenerUrl = "copy-past-url-from-logic-app-http-request-listener";
             var httpClient = new HttpClient();
-            var response = await httpClient.PostAsync(url, new StringContent(payload));
+            var response = await httpClient.PostAsync(logicAppHttpRequestListenerUrl, content);
+            var responseString = await response.Content.ReadAsStringAsync();
 
+            log.LogInformation("Response received {response}", responseString);
             //log.LogInformation("Event received {type} {subject}: {data}", cloudEvent.Type, cloudEvent.Subject, cloudEvent.Data.ToString());
         }
     }
